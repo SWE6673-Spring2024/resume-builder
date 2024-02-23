79a6b3148ca95f0b5e1e7d88344e4c334c9d363e
Reed McCallum <donald.mccallum20@t-mobile.com>
2024-02-23 11:49:43 -0500

    init resume-builder-ui javafx module

---
+++ resume-builder-ui/src/main/java/org/ksu/swe6673/testinpeace/resumebuilderui/HelloController.java
@@ -0,0 +1,14 @@
+package org.ksu.swe6673.testinpeace.resumebuilderui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
