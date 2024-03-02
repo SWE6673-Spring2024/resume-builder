e77be563cc179607b5a336e16d9e08192793054c
reedmccallum <14640761+reedmccallum@users.noreply.github.com>
2024-03-01 23:02:10 -0500

    init test module

---
+++ src/test/java/org/ksu/swe6673/testinpeace/resumebuilder/HelloApplicationTest.java
@@ -0,0 +1,62 @@
+package org.ksu.swe6673.testinpeace.resumebuilder;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HelloApplicationTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void launch() {
+    }
+
+    @Test
+    void testLaunch() {
+    }
+
+    @Test
+    void init() {
+    }
+
+    @Test
+    void stop() {
+    }
+
+    @Test
+    void getHostServices() {
+    }
+
+    @Test
+    void getParameters() {
+    }
+
+    @Test
+    void notifyPreloader() {
+    }
+
+    @Test
+    void getUserAgentStylesheet() {
+    }
+
+    @Test
+    void setUserAgentStylesheet() {
+    }
+
+    @Test
+    void start() {
+    }
+
+    @Test
+    void main() {
+    }
+}
---
+++ src/test/java/org/ksu/swe6673/testinpeace/resumebuilder/HelloControllerTest.java
@@ -0,0 +1,22 @@
+package org.ksu.swe6673.testinpeace.resumebuilder;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HelloControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void onHelloButtonClick() {
+    }
+}
