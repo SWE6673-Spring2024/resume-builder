d20d6b0449efbe4d56b122305828bd6544212b49
reedmccallum <14640761+reedmccallum@users.noreply.github.com>
2024-03-02 13:07:35 -0500

    add to scene graph

--- src/test/java/org/ksu/swe6673/testinpeace/resumebuilder/FormControllerTest.java
+++ src/test/java/org/ksu/swe6673/testinpeace/resumebuilder/FormControllerTest.java
@@ -1,8 +1,13 @@
 package org.ksu.swe6673.testinpeace.resumebuilder;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.testfx.*;
 import org.testfx.api.FxToolkit;
 import org.testfx.framework.junit5.ApplicationTest;
 import javafx.scene.input.MouseButton;
@@ -10,6 +15,14 @@ import javafx.scene.input.MouseButton;
 import static org.junit.jupiter.api.Assertions.*;
 
 class FormControllerTest  extends ApplicationTest {
+
+    @Override
+    public void start (Stage stage) throws Exception {
+        Parent mainNode = FXMLLoader.load(FormController.class.getResource("form.fxml"));
+        stage.setScene(new Scene(mainNode));
+        stage.show();
+        stage.toFront();
+    }
 
     @BeforeEach
     void setUp() {
