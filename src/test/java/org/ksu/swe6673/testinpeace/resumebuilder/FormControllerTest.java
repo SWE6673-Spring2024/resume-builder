39b536eda7887bf383dba8c2d81970cfb6e77c7b
Todd Spainhour <59629794+ToddSpainhour@users.noreply.github.com>
2024-03-02 12:01:40 -0600

    extend applicationTest
    extend application test

--- src/test/java/org/ksu/swe6673/testinpeace/resumebuilder/FormControllerTest.java
+++ src/test/java/org/ksu/swe6673/testinpeace/resumebuilder/FormControllerTest.java
@@ -3,10 +3,13 @@ package org.ksu.swe6673.testinpeace.resumebuilder;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.testfx.api.FxToolkit;
+import org.testfx.framework.junit5.ApplicationTest;
+import javafx.scene.input.MouseButton;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class FormControllerTest {
+class FormControllerTest  extends ApplicationTest {
 
     @BeforeEach
     void setUp() {
@@ -18,5 +21,33 @@ class FormControllerTest {
 
     @Test
     void onSubmitButtonClick() {
+    }
+
+    void TwoPlusTwoEqualsFour() {
+        //clickOn();
+        int firstTwo = 2;
+        int secondTwo = 2;
+
+        int actual = firstTwo + secondTwo;
+        int expected = 4;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    void FirstNameNullValue(){
+
+        FormController myFormController = new FormController();
+
+        clickOn("#firstName");
+        write(null);
+
+        // submit form
+        clickOn("#submitButton");
+
+        // expected
+
+        // actual
+
+        assertEquals(null, myFormController.firstName.getText());
     }
 }
