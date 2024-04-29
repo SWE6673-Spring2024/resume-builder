03c2ce7c6eed599ab70ae63ce05104a0be2f9dba
Reed McCallum <14640761+reedmccallum@users.noreply.github.com>
2024-04-29 12:37:58 -0400

    Surprise implementation (#4)
    * first commit
    
    * working formUITests
    
    * added working pdf generated test
    
    * tried getting non-working string values working as per textfield test
    
    * re-added function in order to remove commented out code
    
    * upload implementation team's feedback on our test suite
    
    ---------
    
    Co-authored-by: Matthew Maravilla <matthewmaravilla@Matthews-Laptop.local>

--- src/main/java/org/ksu/swe6673/testinpeace/resumebuilder/FormController.java
+++ src/main/java/org/ksu/swe6673/testinpeace/resumebuilder/FormController.java
@@ -1,5 +1,9 @@
 package org.ksu.swe6673.testinpeace.resumebuilder;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.input.Clipboard;
+import javafx.scene.input.KeyCode;
 import org.apache.pdfbox.pdmodel.PDDocument;
 
 import javafx.fxml.FXML;
@@ -7,6 +11,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import org.apache.pdfbox.pdmodel.PDPage;
 
 public class FormController {
     @FXML
@@ -72,18 +77,69 @@ public class FormController {
         reset();
     }
 
+    static String makeControlCharactersVisible(String s) {
+        if (s == null) {
+            return s;
+        }
+
+        int len = s.length();
+        StringBuilder visible = new StringBuilder(len);
+
+        for (int i = 0; i < len; i++) {
+            char c = s.charAt(i);
+            visible.append(c >= 32 || c == '\n' ? c : (char) (c + 0x2400));
+            visible.append(c >= 32 || c == '\r' ? c : (char) (c + 0x000D));
+            visible.append(c >= 32 || c == '\t' ? c : (char) (c + 0x0009));
+            System.out.println(c);
+        }
+
+        return visible.toString();
+    }
+
     public void submit() {
         // TODO: save updated form values to inject into PDF
+        firstName = new TextField() {
+            @Override
+            public void paste() {
+                String text = Clipboard.getSystemClipboard().getString();
+                replaceSelection(makeControlCharactersVisible(text));
+            }
+        };
         submitLabel.setText("Submitted");
     }
 
     public void reset() {
         // TODO: clear form values
+        firstName.setText(null);
+        lastName.setText(null);
+        addressStreet.setText(null);
+        addressUnit.setText(null);
+        addressZip.setText(null);
+        addressCity.setText(null);
+        email.setText(null);
+        linkedin.setText(null);
+        github.setText(null);
+        phone.setText(null);
+        workHistory.setText(null);
+        workYears.setText(null);
+        company.setText(null);
+        role.setText(null);
+        description.setText(null);
+        education.setText(null);
+        schoolYears.setText(null);
+        degree.setText(null);
+        school.setText(null);
+        gpa.setText(null);
+        status.setText(null);
+        major.setText(null);
     }
 
     public PDDocument exportPdf() {
         // TODO: create PDF with form values
-        return new PDDocument();
+        PDDocument doc = new PDDocument();
+        PDPage page = new PDPage();
+        doc.addPage(page);
+        return doc;
     }
 
 
